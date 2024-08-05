f14ad8efb0a7d4b1ebc0c148b830a44dc1403808
Le Trung Kien <kienlt@smartosc.com>
2024-08-05 16:00:57 +0700

    Menu

---
+++ application/views/qlhs3/default/components/sidebar-menu.php
@@ -0,0 +1,98 @@
+<ul class="nav nav-sidebar nav-pills flex-column" data-widget="treeview" role="menu" data-accordion="false">
+  <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+  <li class="nav-item menu-open">
+    <a class="nav-link active" href="/home"><i class="fas fa-tachometer-alt nav-icon"></i>
+    <p>
+      Tổng quan
+    </p></a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/home"><i class="fas fa-copy nav-icon"></i>
+    <p>
+      Danh sách lớp
+    </p></a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/home"><i class="fas fa-copy nav-icon"></i>
+    <p>
+      Thời khóa biểu
+    </p></a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#"><i class="fas fa-copy nav-icon"></i>
+    <p>
+      Quản lý thu chi <i class="fas fa-angle-left right"></i> <span class="badge badge-info right">2</span>
+    </p></a>
+    <ul class="nav nav-treeview">
+      <li class="nav-item">
+        <a class="nav-link" href="pages/layout/top-nav-sidebar.html"><i class="far fa-circle nav-icon"></i>
+        <p>
+          Các khoản đã thu
+        </p></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/layout/boxed.html"><i class="far fa-circle nav-icon"></i>
+        <p>
+          Các khoản đã chi
+        </p></a>
+      </li>
+    </ul>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#"><i class="fas fa-copy nav-icon"></i>
+    <p>
+      Học viên <i class="fas fa-angle-left right"></i> <span class="badge badge-info right">7</span>
+    </p></a>
+    <ul class="nav nav-treeview">
+      <li class="nav-item">
+        <a class="nav-link" href="pages/layout/top-nav-sidebar.html"><i class="far fa-circle nav-icon"></i>
+        <p>
+          Học viên đang học
+        </p></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/layout/boxed.html"><i class="far fa-circle nav-icon"></i>
+        <p>
+          Danh sách học bù
+        </p></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/layout/boxed.html"><i class="far fa-circle nav-icon"></i>
+        <p>
+          Danh sách bổ trợ
+        </p></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/layout/boxed.html"><i class="far fa-circle nav-icon"></i>
+        <p>
+          Danh sách chuyển phí
+        </p></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/layout/boxed.html"><i class="far fa-circle nav-icon"></i>
+        <p>
+          Học viên chờ
+        </p></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/layout/boxed.html"><i class="far fa-circle nav-icon"></i>
+        <p>
+          Học viên nghỉ
+        </p></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/layout/boxed.html"><i class="far fa-circle nav-icon"></i>
+        <p>
+          Bảo lưu
+        </p></a>
+      </li>
+    </ul>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/home"><i class="fas fa-copy nav-icon"></i>
+    <p>
+      Giáo viên
+    </p></a>
+  </li>
+</ul>
---
+++ application/views/qlhs3/template/default/components/sidebar-menu.php
@@ -0,0 +1,88 @@
+<nav.sidebar>
+    <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+    <nav.item class="menu-open">
+        <nav.link.with.icon href="/home" icon="tachometer-alt" class="active">Tổng quan</nav.link.with.icon>
+    </nav.item>
+    <nav.item>
+        <nav.link.with.icon href="/home" icon="copy">Danh sách lớp</nav.link.with.icon>
+    </nav.item>
+    <nav.item>
+        <nav.link.with.icon href="/home" icon="copy">Thời khóa biểu</nav.link.with.icon>
+    </nav.item>
+    <nav.item>
+        <nav.link.with.icon href="#" icon="copy">
+            Quản lý thu chi
+            <fas icon="angle-left right" />
+            <badge type="info" class="right">2</badge>
+        </nav.link.with.icon>
+        <ul class="nav nav-treeview">
+            <nav.item>
+                <nav.link href="pages/layout/top-nav-sidebar.html">
+                    <far icon="circle nav-icon" />
+                    <p>Các khoản đã thu</p>
+                </nav.link>
+            </nav.item>
+            <nav.item>
+                <nav.link href="pages/layout/boxed.html">
+                    <far icon="circle nav-icon" />
+                    <p>Các khoản đã chi</p>
+                </nav.link>
+            </nav.item>
+        </ul>
+    </nav.item>
+    <nav.item>
+        <nav.link.with.icon href="#" icon="copy">
+            Học viên
+            <fas icon="angle-left right" />
+            <badge type="info" class="right">7</badge>
+        </nav.link.with.icon>
+        <ul class="nav nav-treeview">
+            <nav.item>
+                <nav.link href="pages/layout/top-nav-sidebar.html">
+                    <far icon="circle nav-icon" />
+                    <p>Học viên đang học</p>
+                </nav.link>
+            </nav.item>
+            <nav.item>
+                <nav.link href="pages/layout/boxed.html">
+                    <far icon="circle nav-icon" />
+                    <p>Danh sách học bù</p>
+                </nav.link>
+            </nav.item>
+            <nav.item>
+                <nav.link href="pages/layout/boxed.html">
+                    <far icon="circle nav-icon" />
+                    <p>Danh sách bổ trợ</p>
+                </nav.link>
+            </nav.item>
+            <nav.item>
+                <nav.link href="pages/layout/boxed.html">
+                    <far icon="circle nav-icon" />
+                    <p>Danh sách chuyển phí</p>
+                </nav.link>
+            </nav.item>
+            <nav.item>
+                <nav.link href="pages/layout/boxed.html">
+                    <far icon="circle nav-icon" />
+                    <p>Học viên chờ</p>
+                </nav.link>
+            </nav.item>
+            <nav.item>
+                <nav.link href="pages/layout/boxed.html">
+                    <far icon="circle nav-icon" />
+                    <p>Học viên nghỉ</p>
+                </nav.link>
+            </nav.item>
+            <nav.item>
+                <nav.link href="pages/layout/boxed.html">
+                    <far icon="circle nav-icon" />
+                    <p>Bảo lưu</p>
+                </nav.link>
+            </nav.item>
+        </ul>
+    </nav.item>
+    <nav.item>
+        <nav.link.with.icon href="/home" icon="copy">Giáo viên</nav.link.with.icon>
+    </nav.item>
+</nav.sidebar>
